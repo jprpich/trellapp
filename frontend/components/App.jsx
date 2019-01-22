0ea51f580656953b09032652777ccd0c06518605
jprpich <joshuaprpich@gmail.com>
2019-01-22 15:21:02 -0500

    protect front-end routes

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -5,6 +5,7 @@ import GreetingContainer from './greeting/greeting_container';
 import LoginFormContainer from './session_form/login_form_container';
 import SignupFormContainer from './session_form/signup_form_container';
 
+import { AuthRoute } from '../util/route_util';
 
 const App = () => {
   return (
@@ -12,8 +13,8 @@ const App = () => {
       <h1>Trellapp</h1>
       <GreetingContainer />
 
-      <Route path="/login" component={LoginFormContainer} />
-      <Route path="/signup" component={SignupFormContainer} />
+      <AuthRoute path="/login" component={LoginFormContainer} />
+      <AuthRoute path="/signup" component={SignupFormContainer} />
     </div>
   )
 }
