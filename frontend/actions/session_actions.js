5470bbb9ca584608332c641acafe05cb935565c0
jprpich <joshuaprpich@gmail.com>
2019-01-22 14:26:42 -0500

    add auth actions and reducers

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,51 @@
+import * as ApiUtil from '../util/session_api_util';
+
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER";
+export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
+
+export const login = user => dispatch => {
+  ApiUtil.login(user).then(user => {
+    dispatch(receiveCurrentUser(user));
+  },
+    errors => {
+      dispatch(receiveErrors(errors));
+    }
+  );
+};
+
+export const logout = () => dispatch => {
+  ApiUtil.logout().then(() => {
+    dispatch(logoutCurrentUser());
+  });
+};
+
+export const signup = user => dispatch => {
+  ApiUtil.signup(user).then(user => {
+    dispatch(receiveCurrentUser(user));
+  },
+    errors => {
+      dispatch(receiveErrors(errors));
+    }
+  );
+};
+
+const receiveCurrentUser = user => {
+
+  return ({
+    type: RECEIVE_CURRENT_USER,
+    user
+  });
+};
+
+const logoutCurrentUser = () => ({
+  type: LOGOUT_CURRENT_USER,
+});
+
+const receiveErrors = (errors) => {
+
+  return ({
+    type: RECEIVE_SESSION_ERRORS,
+    errors
+  });
+};
---
+++ frontend/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Route } from 'react-router-dom';
+
+
+
+
+const App = () => {
+  return (
+    <div>
+      <h1>Trellapp</h1>
+    </div>
+  )
+}
+
+export default App;
