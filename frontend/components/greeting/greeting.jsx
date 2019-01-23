62f35359803166857684209cd6cab3a477a8e33a
jprpich <joshuaprpich@gmail.com>
2019-01-23 17:52:42 -0500

    add session form styles

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -4,18 +4,18 @@ import { Link } from 'react-router-dom';
 const Greeting = ({ currentUser, logout }) => {
   if (currentUser) {
     return (
-      <div>
+      <div className="greeting-component">
         <h2>Welcome, {currentUser.email}!</h2>
         <button onClick={logout}>Logout</button>
       </div>
     )
   } else {
     return (
-      <div>
+      <div className="greeting-component"> 
         <h1>Trellapp</h1>
         <h2>Trello lets you work more collaboratively and get more done.</h2>
         <p>Trello’s boards, lists, and cards enable you to organize and prioritize your projects in a fun, flexible and rewarding way.</p>
-        <button className="app-signup-button">Sign Up – It’s Free!</button>
+        <Link className="app-signup-button" to="/signup">Sign Up – It’s Free!</Link>
       </div>
     )
   }
--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -7,7 +7,8 @@ class SessionForm extends React.Component {
     super(props);
     this.state = {
       email: "",
-      password: ""
+      password: "",
+      disabled: true 
     };
   }
 
@@ -28,31 +29,91 @@ class SessionForm extends React.Component {
   updateEmail(e) {
     this.setState({
       email: e.target.value
-    })
+    }, () => {
+      if (this.state.email.length > 0 && this.state.password.length > 0) {
+        this.setState({
+          disabled: false
+        });
+      }
+    });
   }
 
   updatePassword(e) {
     this.setState({
       password: e.target.value
-    })
+    }, () => {
+      if (this.state.email.length > 0 && this.state.password.length > 0) {
+        this.setState({
+          disabled: false
+        });
+      }
+    });
   }
 
   render() {
-    const errors = this.props.errors.map((error, idx) => {
-      return <li key={idx}>{error}</li>
-    })
+    let error; 
+    if (this.props.errors.length >= 1) {
+      error = <p className="error-message">{this.props.errors[0]}</p>
+    } else {
+      error = ""
+    }
+
     return (
-      <div>        
-        <Link to={this.props.formType === "signup" ? "/login" : "/signup"}>{this.props.formType === "signup" ? "Log in" : "Sign Up"}</Link>
-        <h2>{this.props.formType === "signup" ? "Sign up" : "Log in"}</h2>
+      <div className="session-form">        
+        {error}
+        <h1>{this.props.formType === "signup" ? "Create a Trello Account" : "Log in to Trello"}</h1>
+        <div className="create-account-quiet">
+          or <Link 
+               to={this.props.formType === "signup" ? "/login" : "/signup"}>{this.props.formType === "signup" ? "sign in to your account" : "create an account"}
+             </Link>
+        </div>
+        
         <form onSubmit={this.handleSubmit.bind(this)}>
-          <input type="email" onChange={this.updateEmail.bind(this)} value={this.state.email} placeholder="email" />
-          <input type="password" onChange={this.updatePassword.bind(this)} value={this.state.password} placeholder="password" />
-          <button>Submit!</button>
+          <label htmlFor="email">Email</label>
+          <input 
+            id="email" 
+            type="email" 
+            onChange={this.updateEmail.bind(this)} 
+            value={this.state.email} 
+            placeholder="e.g, leela@planetexpress.nnyc" 
+          />
+  
+          <label htmlFor="password">Password</label>
+          <input 
+            id="password" 
+            type="password" 
+            onChange={this.updatePassword.bind(this)} 
+            value={this.state.password} 
+            placeholder="e.g,***********" 
+          />
+          
+          <input 
+            type="submit" 
+            disabled={(this.state.disabled && this.props.formType === "signup") ? "disabled" : ""} 
+            className={(this.state.disabled && this.props.formType === "signup") ? "disabled" : "real-user"} 
+            value={this.props.formType === "signup" ? "Create New Account" : "Log In"}
+          />
+
+          <span className="demo-user-span">
+            <i className="fas fa-user"></i><input type="submit" value="Sign up with Demo User" className="demo-user" />
+          </span> 
         </form>
-        <ul>
-          {errors}
-        </ul>
+       
+        <footer>
+          <ul>
+            <li>
+              <a href="https://github.com/jprpich" target="_blank">Joshua Prpich Github</a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com" target="_blank">LinkedIn</a>
+            </li>
+            <li>
+              <a href="https://www.facebook.com/" target="_blank">Facebook</a>
+            </li>
+          </ul>
+          <p>© Copyright 2019. All rights reserved.</p>
+        </footer>
+        
       </div>
     )
   }
