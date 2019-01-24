7ccd36efed2a82d1b4c18098a4a7277b42f3f48b
jprpich <joshuaprpich@gmail.com>
2019-01-24 11:06:01 -0500

    add hero div

--- frontend/components/greeting/greeting_main.jsx
+++ frontend/components/greeting/greeting_main.jsx
@@ -9,10 +9,16 @@ class GreetingMain extends React.Component {
   render() {
     return (
       <div className="greeting-main">
-        <h2>Trello lets you work more collaboratively and get more done.</h2>
-        <p>Trello’s boards, lists, and cards enable you to organize and prioritize your projects in a fun, flexible and rewarding way.</p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe quam error dicta, expedita quibusdam magni omnis maxime, necessitatibus rem incidunt facilis, vel qui obcaecati itaque pariatur neque amet sed optio.</p>
-        <Link className="app-signup-button" to="/signup">Sign Up – It’s Free!</Link>
+        <div className="layout-two-col">
+          <div className="hero-text">
+            <h1>Trellapp lets you work more collaboratively and get more done.</h1>
+            <p className="big">Trellapp’s boards, lists, and cards enable you to organize and prioritize your projects in a fun, flexible and rewarding way.</p>
+            <Link className="signup-button" to="/signup">Sign Up – It’s Free!</Link>
+          </div>
+          <div className="hero-image">
+            <img src="https://d2k1ftgv7pobq7.cloudfront.net/meta/p/res/images/308998dcb3ed5ab3d01217a4d24ffa03/hero-a.svg" alt=""/>
+          </div>
+        </div>
       </div>
     )
   }
