c7ed2a35b7e875c038260abb87e527ded315007c
Joshua <joshuaprpich@gmail.com>
2019-02-05 23:09:06 -0500

    change import from list_actions to card api util

--- frontend/components/lists/list_index_container.js
+++ frontend/components/lists/list_index_container.js
@@ -1,7 +1,7 @@
 import { connect } from 'react-redux';
 import Lists from './list_index';
 import { deleteList, receiveLists } from '../../actions/list_actions';
-import { updateCard } from '../../actions/card_actions';
+import { updateCard } from '../../util/card_api_util';
 import {withRouter} from 'react-router-dom';
 
 
@@ -16,7 +16,7 @@ const msp = (state, ownProps) => {
 const mdp = dispatch => ({
   deleteList: (listId) => dispatch(deleteList(listId)),
   receiveLists: (id) => dispatch(receiveLists(id)),
-  updateCard: (card) => dispatch(updateCard(card))
+  updateCard: (card) => updateCard(card)
 });
 
 export default withRouter(connect(msp, mdp)(Lists));
