978fa2bc678c36a6d66b6c5b37b794623a4ba007
Joshua <joshuaprpich@gmail.com>
2019-04-08 09:41:36 -0400

    change picture

--- frontend/components/greeting/greeting_main.jsx
+++ frontend/components/greeting/greeting_main.jsx
@@ -15,12 +15,12 @@ class GreetingMain extends React.Component {
       <div className="greeting-main">
         <div className="layout-two-col">
           <div className="hero-text">
-            <h1>Trellapp lets you work more collaboratively and get more done.</h1>
-            <p className="big">Trellapp’s boards, lists, and cards enable you to organize and prioritize your projects in a fun, flexible and rewarding way.</p>
+            <h1>Create multiple boards and track a single projects lists.</h1>
+            <p className="big">Boards contain lists which contain cards. Cards have a title and description. Cards can be moved from one list to another.</p>
             <Link className="signup-button" to="/signup">Sign Up – It’s Free!</Link>
           </div>
           <div className="hero-image">
-            <img src="https://d2k1ftgv7pobq7.cloudfront.net/meta/p/res/images/308998dcb3ed5ab3d01217a4d24ffa03/hero-a.svg" alt=""/>
+            <img src="https://cdn.elearningindustry.com/wp-content/uploads/2016/06/5-creative-ways-effective-elearning-experience.png" alt=""/>
           </div>
         </div>
         <footer>
